5459219ef5625320edbcc525abbc8c1cded104b2
b1017113 <67138949+b1017113@users.noreply.github.com>
2020-08-25 18:20:57 +0900

    #1 clean .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import "firebase/auth";
 import 'firebase/analytics'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCxUUXsXDqyzMSbO6WtESfEjvMrCYrkQs4",
-    authDomain: "impl-intern.firebaseapp.com",
-    databaseURL: "https://impl-intern.firebaseio.com",
-    projectId: "impl-intern",
-    storageBucket: "impl-intern.appspot.com",
-    messagingSenderId: "732744868364",
-    appId: "1:732744868364:web:ac792833ffadaae5588429",
-    measurementId: "G-TTJKQTT7DB"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
