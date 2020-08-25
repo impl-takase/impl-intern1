51b019b93d2faaac4fd3091625ec1a386fcef219
b1017113 <67138949+b1017113@users.noreply.github.com>
2020-08-25 16:20:00 +0900

    #1 create firebase

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import firebase from './firebase';
+
+firebase.firestore().collection('item').add({
+  id:0,
+  task:'Reactを勉強する',
+  explanation: 'react勉強'
+}
+)
+
+
+function App() {
+
+
+  return (
+
+    <p>aa</p>
+    
+  );
+}
+
+export default App;
